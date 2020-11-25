bf2368b8d3c8ba3ddc09b5a2c4b5583103984cfc
sheva77 <andreyshevchenko1177@gmail.com>
2020-11-26 01:31:45 +0200

    hw05 -avg

---
+++ js/05_functions_scopes/hw05_functions.js
@@ -0,0 +1,34 @@
+
+// a
+// Напишите функцию a, которая просто является коротким именем для alert.
+
+function a(message) {
+    var res = alert(message)
+    return res
+}
+
+a(`Привет!\n` + a)
+
+
+
+
+
+// cube
+// Напишите функцию cube, которая возвращает число в третьей степени:
+
+const cube = function (toCube) {
+    var cubeIs = toCube * toCube * toCube;
+    return cubeIs
+};
+
+alert(`Your number in cube iz: ` + cube(+prompt("Input number:")));
+
+
+
+
+// avg2
+// Напишите функцию avg2, которая рассчитывает среднюю для двух чисел:
+
+const avg2 = ([a = 0, b = 0]) => (a + (b || a)) / 2;
+
+alert(`The mean is: ` + avg2((prompt(`Введите два числа через пробел:`) || "0").split(' ').filter(a => !isNaN(a)).map(a => +a)));
