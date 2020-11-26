90b0bd2b7f4b88e68fca888004eb65d5f9d5c02a
sheva77 <andreyshevchenko1177@gmail.com>
2020-11-26 03:07:22 +0200

    hw05 sum3

--- js/05_functions_scopes/hw05_functions.js
+++ js/05_functions_scopes/hw05_functions.js
@@ -9,9 +9,9 @@
 function a(message) {
     var res = alert(message)
     return res
-}
+};
 
-a(`Привет!\n` + a)
+a(`Привет!\n` + a);
 
 
 
@@ -22,11 +22,12 @@ a(`Привет!\n` + a)
 
 
 const cube = function (toCube) {
-    var cubeIs = toCube * toCube * toCube;
+    var cubeIs = toCube * toCube * toCube;   //  Math.pow(toCube, 3);
     return cubeIs
 };
 
 alert(`Your number in cube iz: ` + cube(+prompt("Input number:")));
+
 
 
 
@@ -40,8 +41,38 @@ alert(`Your number in cube iz: ` + cube(+prompt("Input number:")));
 
 
 const avg2 = () => {
-    [a = 0, b = 0] = (prompt(`Введите два числа через пробел:`) || "0").split(' ').filter(a => !isNaN(a)).map(a => +a);
-    return (a + (b || a)) / 2;
+    [x1 = 0, x2 = 0] = (prompt(`Введите два числа через пробел:`) || "0").split(' ').filter(a => !isNaN(a)).map(a => +a);
+    return (x1 + (x2 || x1)) / 2;
 };
 
 alert(`The mean is: ` + avg2());
+
+
+
+
+
+//  sum3
+// Напишите функцию sum3 для суммирования 3 чисел:
+// Обратите внимание, что sum3 от двух параметров тоже работает корректно.
+
+// Если просто без заморочек
+// const sum3 = (x1 = 0, x2 = 0, x3 = 0) => +x1 + +x2 + +x3;
+
+function inpNumberArr() {
+    var term = [];
+    var inp;
+
+    do {
+        inp = prompt("Вводите числа пока не надоест.\nМы их все просуммируем");
+        if (!isNaN(+inp) && inp !== null) { term.push(+inp) };
+    } while (!isNaN(+inp) && inp !== null);
+    return term;
+}
+
+alert('Сумма всех чисел = ' + inpNumberArr().reduce((ar1, ar2) => ar1 + ar2));
+
+
+
+
+
+
