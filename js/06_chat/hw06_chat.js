89a3d7476982a7e9302ff6ae6b728856365d088e
sheva77 <andreyshevchenko1177@gmail.com>
2020-11-30 23:25:07 +0200

    hw_chat server-fix final. I hope. ))

--- js/06_chat/hw06_chat.js
+++ js/06_chat/hw06_chat.js
@@ -1,4 +1,14 @@
+const socket = io("http://localhost:4000");
 users = []; // кто тут в чате
+
+let nick;
+while (!(nick = prompt("Введите свой ник:"))) {}
+nickId.value = nick;
+socket.emit("msg", {
+    nick: nickId.value,
+    message: "---Я тут новенький!!!---",
+});
+nickId.setAttribute("readonly", "readonly");
 
 // отправка сообщения в чат при нажатии Enter в поле ввода сообщения
 msgId.addEventListener("keydown", function (e) {
@@ -8,15 +18,22 @@ msgId.addEventListener("keydown", function (e) {
 });
 
 function reciveMsg(msg) {
+    debugger;
     console.log(msg);
-    if (msg.nick !== "--Администрация--") {
-        chatWindow.innerHTML += "<br/><b>" + msg.nick + ":</b>  " + msg.message;
-    } else {
+    if (msg.nick === "--Администрация--") {
         chatWindow.innerHTML += "<br/>" + msg.nick + ":  " + msg.message;
+        users.length = 0;
+        socket.emit("msg", {
+            nick: nickId.value,
+            message: "$$_iAm_$$",
+        });
+    } else if (msg.message !== "$$_iAm_$$") {
+        chatWindow.innerHTML += "<br/><b>" + msg.nick + ":</b>  " + msg.message;
     }
+
     chatWindow.scrollTop = chatWindow.scrollHeight; // прокрутка скрола чата
 
-    if (!~users.indexOf(msg.nick)) {
+    if (!~users.indexOf(msg.nick) && msg.nick !== "--Администрация--") {
         users.push(msg.nick);
         users.sort((a, b) => (a < b && -1) || 1);
         whoIsHere.innerHTML = "Кто у нас тут в чате:";
@@ -27,7 +44,6 @@ function reciveMsg(msg) {
     }
 }
 
-const socket = io("http://localhost:4000");
 socket.on("msg", (msg) => reciveMsg(msg));
 
 sendId.onclick = () => {
