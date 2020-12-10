2dc7eb527e7e1580bd3cebf1f265a588a85f2414
sheva77 <andreyshevchenko1177@gmail.com>
2020-12-10 21:37:06 +0200

    hw_10_Closures in process

--- js/10_Closures/hw_10_closures.js
+++ js/10_Closures/hw_10_closures.js
@@ -103,3 +103,17 @@ finalCountdown2(5);
 // Изучите встроенную функцию bind, и сделайте свою версию,
 // которая позволит определить "значение по умолчанию" не только для первых параметров,
 // но для любых других, например для степени в Math.pow:
+
+var animals = [
+    { species: "Лев", name: "Король" },
+    { species: "Кит", name: "Фэйл" },
+];
+
+for (var i = 0; i < animals.length; i++) {
+    (function (i) {
+        this.print = function () {
+            console.log("#" + i + " " + this.species + ": " + this.name);
+        };
+        this.print();
+    }.call(animals[i], i));
+}
