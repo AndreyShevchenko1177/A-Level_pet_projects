64b53793fc79bc0fed72b5009501a143e44a30f8
sheva77 <andreyshevchenko1177@gmail.com>
2021-01-04 12:48:21 +0200

    hw20

--- js/20_async_await_svetofor/hw20.js
+++ js/20_async_await_svetofor/hw20.js
@@ -1,4 +1,4 @@
-const delay = (ms) => new Promise((ok) => setTimeout(() => ok(), ms * 1000));
+const delay = (sec) => new Promise((ok) => setTimeout(() => ok(sec), sec * 1000));
 
 async function trafficLight(domElement, { redSec = 5, yellowSec = 2, greenSec = 3 } = {}) {
     while (true) {
@@ -41,3 +41,40 @@ let domEventPromise = (domElement, eventName) =>
     });
 
 knopka2.onclick = () => domEventPromise(knopka, "click").then((e) => console.log("event click happens", e));
+
+//
+
+// PedestrianTrafficLight
+// Напишите упрощенный светофор для пешеходов
+// Stage 2
+// Используя Promise.race, domEventPromise и кнопку в DOM сделайте пешеходный светофор с кнопкой,
+// который так же переключается по времени периодически.
+// Stage 3
+// Не давайте возможности пешеходам сильно наглеть - предусмотрите задержку, после которой будет работать кнопка.
+
+// lights2red.style.background = "red";
+
+async function pedestrianTrafficLight(domElement, { redSec = 10, greenSec = 2 } = {}) {
+    domElement.children[0].style.background = "";
+    domElement.children[1].style.background = "green";
+    await delay(greenSec);
+    domElement.children[1].style.background = "";
+
+    domElement.children[0].style.background = "red";
+    await delay(redSec);
+    domElement.children[0].style.background = "";
+    return "semafor";
+}
+
+async function pedestrianManage(domElement) {
+    debugger;
+
+    while (true) {
+        // let res = await Promise.race([domEventPromise(goButton, "click"), delay(5)]);
+        let res = await Promise.race([domEventPromise(goButton, "click"), pedestrianTrafficLight(domElement)]);
+
+        console.log(res);
+    }
+}
+
+pedestrianManage(lights2);
