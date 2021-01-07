d6c1b0cd2dc9f409bcca53cc2b6a222d8cfcd925
sheva77 <andreyshevchenko1177@gmail.com>
2021-01-07 21:20:32 +0200

    hw20 trafficLight speedtest all done

--- js/20_async_await_svetofor/hw20.js
+++ js/20_async_await_svetofor/hw20.js
@@ -1,19 +1,19 @@
-const delay = (sec) => new Promise((ok) => setTimeout(() => ok(sec), sec * 1000));
+const delaySec = (sec) => new Promise((ok) => setTimeout(() => ok(sec), sec * 1000));
 
 async function trafficLight(domElement, { redSec = 5, yellowSec = 2, greenSec = 3 } = {}) {
     while (true) {
         domElement.children[2].style.background = "green";
-        await delay(greenSec);
+        await delaySec(greenSec);
         domElement.children[2].style.background = "";
 
         domElement.children[1].style.background = "yellow";
-        await delay(yellowSec);
+        await delaySec(yellowSec);
         domElement.children[1].style.background = "";
 
         domElement.children[0].style.background = "red";
-        await delay(redSec - 1);
+        await delaySec(redSec - 1);
         domElement.children[1].style.background = "yellow";
-        await delay(1);
+        await delaySec(1);
         domElement.children[0].style.background = "";
         domElement.children[1].style.background = "";
     }
@@ -58,12 +58,12 @@ function innerTimer(domElement, sec) {
 async function pedestrianTrafficLight(domElement, { redSec = 10, greenSec = 3 } = {}) {
     domElement.children[1].style.background = "lightgreen";
     innerTimer(domElement.children[1], greenSec);
-    await delay(greenSec);
+    await delaySec(greenSec);
     domElement.children[1].style.background = "";
 
     domElement.children[0].style.background = "red";
     innerTimer(domElement.children[0], redSec);
-    await delay(redSec);
+    await delaySec(redSec);
     domElement.children[0].style.background = "";
 }
 
@@ -99,7 +99,7 @@ async function pedestrianManageBtn(domElement) {
         domElement.children[1].style.background = "";
         domElement.children[0].style.background = "red";
         innerTimer(domElement.children[0], redTime);
-        await delay(redTime);
+        await delaySec(redTime);
         // domElement.children[0].style.background = "";
         // domElement.children[1].style.background = "lightgreen";
         // return "red";
@@ -110,7 +110,7 @@ async function pedestrianManageBtn(domElement) {
         domElement.children[0].style.background = "";
         domElement.children[1].style.background = "lightgreen";
         innerTimer(domElement.children[1], greenTime);
-        await delay(greenTime);
+        await delaySec(greenTime);
         domElement.children[1].style.background = "";
         domElement.children[0].style.background = "red";
         if (goButton.innerText === "GO") {
@@ -163,3 +163,89 @@ async function pedestrianManageBtn(domElement) {
 }
 
 pedestrianManageBtn(lights2btn);
+
+//
+
+//
+
+// speedtest
+// Написать асинхронную функцию
+// async function speedtest(getPromise, count,parallel=1){
+// ....
+//     return {
+//         duration,
+//         querySpeed, //средняя скорость одного запроса
+//         queryDuration, //
+//         parallelSpeed,
+//         parallelDuration
+//     }
+// }
+
+// speedtest(() => delay(1000), 10, 10 ).then(result => console.log(result))
+
+// // {duration: 10000,
+// // querySpeed: 0.001, //1 тысячная запроса за миллисекунду
+// // queryDuration: 1000, //1000 миллисекунд на один реальный запрос в среднем
+// // parallelSpeed: 0.01  // 100 запросов за 10000 миллисекунд
+// // parallelDuration: 100 // 100 запросов за 10000 миллисекунд
+
+// speedtest(() => fetch('http://swapi.dev/api/people/1').then(res => res.json()), 10, 5)
+// где:
+// count - количество повторов
+// parallel - количество одновременных запросов/промисов в одном повторе
+// getPromise - функция, которая умеет вернуть нужный Вам промис для тестирования скорости его работы
+// которая будет в цикле count раз создавать parallel промисов с помощью переданной функции getPromise,
+// дожидаться выполнения всех parallel промисов, после чего цикл повторяется.
+// Замерить время общее время выполнения, и вычислить:
+// duration, общую длительность работы цикла
+// parallelDuration, среднее время обработки запроса параллельно (за какое время исполнилось parallel*count промисов),
+// paralledSpeed, скорость в запросах в миллисекунду
+// queryDuration, реальное среднее время запроса (отталкиваясь от count и времени работы цикла).
+// querySpeed, реальное средняя скорость запроса
+// Эти переменные вернуть в одном объекте-результате (см. заготовку выше)
+// Для отладки попробуйте на delay (пример выше есть, реальное время будет отличаться на единицы - десятки миллисекунд).
+// Потом можете попробовать на swapi.dev.Не создавайте чрезмерно много параллельных запросов.
+
+const delay = (msec) => new Promise((resolve) => setTimeout(() => resolve(msec), msec));
+
+let setTimer = function () {
+    let timeStart = performance.now();
+    return function () {
+        return performance.now() - timeStart;
+    };
+};
+
+async function speedtest(getPromise, count = 1, parallel = 1) {
+    //
+    let duration, querySpeed, queryDuration, parallelSpeed, parallelDuration;
+
+    let promiseArr = [];
+
+    duration = setTimer();
+    for (let iterations = 0; iterations < count; iterations++) {
+        for (let together = 0; together < parallel; together++) promiseArr.push(getPromise());
+
+        // let timer = setTimer();
+        await Promise.all(promiseArr);
+        // timer = timer();
+    }
+    duration = duration();
+    parallelSpeed = (count * parallel) / duration;
+    parallelDuration = 1 / parallelSpeed;
+    queryDuration = duration / count;
+    querySpeed = 1 / queryDuration;
+
+    return {
+        duration,
+        querySpeed,
+        queryDuration,
+        parallelSpeed,
+        parallelDuration,
+    };
+}
+
+speedtest(() => delay(1000), 10, 10).then((result) => console.log(" delay(1000) ", result));
+
+speedtest(() => fetch("http://swapi.dev/api/people/1").then((res) => res.json()), 10, 5).then((result) =>
+    console.log(" swapi.dev ", result)
+);
