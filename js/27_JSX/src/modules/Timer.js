64e0541ceaba4ae1e9dc3091a98ed6f81bafffcf
sheva77 <andreyshevchenko1177@gmail.com>
2021-03-05 00:25:49 +0200

    comments

--- js/27_JSX/src/modules/Timer.js
+++ js/27_JSX/src/modules/Timer.js
@@ -25,8 +25,7 @@ export const Timer = ({ sec = 0, startWithPause = true }) => {
     if (timeLeft === 0) {
         clockface.current && (clockface.current.style.backgroundColor = "red");
     } else {
-        clockface.current &&
-            (clockface.current.style.backgroundColor = "white");
+        clockface.current && (clockface.current.style.backgroundColor = "white");
     }
 
     const decTime = () => {
@@ -43,9 +42,7 @@ export const Timer = ({ sec = 0, startWithPause = true }) => {
             <span ref = {clockface} style={{ border: "2px solid gray" }}>
                 {`--- ${h}:${m.toString().padStart(2, 0)}:${s.toString().padStart(2, 0)} ---`}
             </span>
-            <button onClick={() => setPause(!pause)}>
-                {(pause && "Continue") || "Pause"}
-            </button>
+            <button onClick={() => setPause(!pause)}>{(pause && "Continue") || "Pause"}</button>
         </>
     );
 };
