21236e04a6e3cb3ac96021a0559843013a91a188
sheva77 <andreyshevchenko1177@gmail.com>
2021-06-19 00:26:20 +0300

    codewars

---
+++ codewars/004__A_Simplistic_TCP_Finite_State_Machine.js
@@ -0,0 +1,35 @@
+function traverseTCPStates(eventList) {
+    let events = {
+        "CLOSED: APP_PASSIVE_OPEN": "LISTEN",
+        "CLOSED: APP_ACTIVE_OPEN": "SYN_SENT",
+        "LISTEN: RCV_SYN": "SYN_RCVD",
+        "LISTEN: APP_SEND": "SYN_SENT",
+        "LISTEN: APP_CLOSE": "CLOSED",
+        "SYN_RCVD: APP_CLOSE": "FIN_WAIT_1",
+        "SYN_RCVD: RCV_ACK": "ESTABLISHED",
+        "SYN_SENT: RCV_SYN": "SYN_RCVD",
+        "SYN_SENT: RCV_SYN_ACK": "ESTABLISHED",
+        "SYN_SENT: APP_CLOSE": "CLOSED",
+        "ESTABLISHED: APP_CLOSE": "FIN_WAIT_1",
+        "ESTABLISHED: RCV_FIN": "CLOSE_WAIT",
+        "FIN_WAIT_1: RCV_FIN": "CLOSING",
+        "FIN_WAIT_1: RCV_FIN_ACK": "TIME_WAIT",
+        "FIN_WAIT_1: RCV_ACK": "FIN_WAIT_2",
+        "CLOSING: RCV_ACK": "TIME_WAIT",
+        "FIN_WAIT_2: RCV_FIN": "TIME_WAIT",
+        "TIME_WAIT: APP_TIMEOUT": "CLOSED",
+        "CLOSE_WAIT: APP_CLOSE": "LAST_ACK",
+        "LAST_ACK: RCV_ACK": "CLOSED",
+    };
+
+    var state = "CLOSED"; // initial state, always
+    // Traversal code goes here
+
+    eventList.forEach((el) => {
+        state = events[`${state}: ${el}`] ? events[`${state}: ${el}`] : "ERROR";
+    });
+
+    return state;
+}
+
+console.log(traverseTCPStates(["APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN"]));
